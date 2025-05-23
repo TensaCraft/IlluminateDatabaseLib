bc5c378838313e75eb6164bd3b680d5a637d0c26
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2025-05-23 11:02:27 +0300

    Update BaseModel.php

--- modules/IlluminateDatabaseLib/BaseModel.php
+++ modules/IlluminateDatabaseLib/BaseModel.php
@@ -8,38 +8,10 @@ use Carbon\Carbon;
 
 class BaseModel extends Model
 {
-    /**
-     * Set Carbon locale on boot.
-     */
     protected static function booted(): void
     {
         parent::booted();
-        Carbon::setLocale(defined('HTML_LANG') ? HTML_LANG : 'en');
-    }
-
-    public function created_ago(?string $column = 'created_at'): string
-    {
-        $date = $this->{$column};
-        if (!$date instanceof Carbon) {
-            try {
-                $date = Carbon::parse($date);
-            } catch (\Exception $e) {
-                return '';
-            }
-        }
-        return $date->diffForHumans();
-    }
-
-    public function updated_ago(?string $column = 'updated_at'): string
-    {
-        $date = $this->{$column};
-        if (!$date instanceof Carbon) {
-            try {
-                $date = Carbon::parse($date);
-            } catch (\Exception $e) {
-                return '';
-            }
-        }
-        return $date->diffForHumans();
+        static::automaticallyEagerLoadRelationships();
+        Carbon::setLocale(defined('LANGUAGE') ? LANGUAGE : 'en_UK');
     }
 }
